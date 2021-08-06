6b18a1bc8cbb9dc095e8a766248d4912fd20e55f
Ekrem Sağlam <ekremsaglammm@gmail.com>
2021-08-06 13:17:13 +0300

    Final Commit

--- Program.cs
+++ Program.cs
@@ -6,26 +6,44 @@ namespace classes
     {
         static void Main(string[] args)
         {
-            Employee calisan = new Employee();
-            calisan.name="Ekrem";
-            calisan.surname="Sağlam";
-            calisan.contactNumber=5365554466;
-            calisan.department="IT";
+            Employee calisan = new Employee("Ekrem", "Sağlam", 5365554466, "IT");
+            Employee calisan2 = new Employee("mahmut", "sağlam");
             calisan.printInfo();
+            calisan2.printInfo();
+            calisan.Name="mehmet";
+
 
         }
     }
 
     class Employee
     {
-        public string name;
-        public string surname;
-        public long contactNumber;
-        public string department;
+        private string name;
+        private string surname;
+        private long contactNumber = 0;
+        private string department = null;
+        public Employee(string name, string surname, long contactNumber, string department)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.contactNumber = contactNumber;
+            this.department = department;
+        }
+        public Employee(string name, string surname)
+        {
+            this.name = name;
+            this.surname = name;
+        }
         public void printInfo()
         {
             Console.WriteLine(name + " " + surname + " isimli çalışanın telefon numarası : " + contactNumber + " deparmanı : " + department);
         }
+
+        public string Name { get => name; set => name = value; }
+        public string Surname { get => surname; set => surname = value; }
+        public long ContactNumber { get => contactNumber; set => contactNumber = value; }
+        public string Department { get => department; set => department = value; }
+
 
 
     }
