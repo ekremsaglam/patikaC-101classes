a3f6d2540c7705e1385695839dd7211932e2f85d
Ekrem Sağlam <ekremsaglammm@gmail.com>
2021-08-06 09:43:51 +0300

    Commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee calisan = new Employee();
+            calisan.name="Ekrem";
+            calisan.surname="Sağlam";
+            calisan.contactNumber=5365554466;
+            calisan.department="IT";
+            calisan.printInfo();
+
+        }
+    }
+
+    class Employee
+    {
+        public string name;
+        public string surname;
+        public long contactNumber;
+        public string department;
+        public void printInfo()
+        {
+            Console.WriteLine(name + " " + surname + " isimli çalışanın telefon numarası : " + contactNumber + " deparmanı : " + department);
+        }
+
+
+    }
+}
